633d8f3d3fa88dcf6b4fa423ba0f31d1fd775342
efrat <efrat@Efrats-MacBook-Air.local>
2019-07-31 22:15:42 +0300

    safe map

--- src/index.js
+++ src/index.js
@@ -16,6 +16,7 @@ async function getUnread() {
       exclude_archived: true,
     },
   });
+  console.log(response.data.channels);
   return response && response.data && response.data.channels && Promise
     .all(response.data.channels
       .map(async channel => axios.get(GET_UNREAD, {
